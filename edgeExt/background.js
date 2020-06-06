1f93aea7478c28fbf6217fa085ad261f11936ef4
bailey bruketta <bruketta.bailey@gmail.com>
2020-06-05 21:32:20 -0500

    converted to edge via Edge Dev Toolkit
    Performs a grey darkmode out of the box. Does not perform with as much functionality as the chrome from which it was converted. First thought is the use of stroage.sync which utlizies user pforiles that would not be present in edge.

---
+++ edgeExt/background.js
@@ -0,0 +1,19 @@
+//the first script that runs. is always running in the background
+console.log("Background running");
+
+chrome.runtime.onInstalled.addListener(function() {
+  //change default background to #303030 when dark mode is implemented
+  chrome.storage.sync.set({background: '#303030', txt: '#ffffff', darkmode: true, layers: 0}, function() {
+    console.log("Default is Dark Mode");
+  });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {schemes: ['https', 'http']}
+        })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
---
+++ edgeExt/content.js
@@ -0,0 +1,58 @@
+console.log("Runs on page load");
+
+const body = document.querySelector("body");
+const firstLayerDivs = document.querySelectorAll("body > div");
+let allElts = document.querySelectorAll("*");
+const images = document.querySelectorAll("img");
+const html = document.querySelector("html");
+
+function divLayer(divs, currNum, endNum, color){
+	if(currNum < endNum){
+		for (div of divs){
+			if(div.tagName === "DIV"){
+				div.style.backgroundColor=color;
+				console.log(div.children || "no more children");
+				if(div.childElementCount){
+					divLayer(div.children, currNum++, endNum, color);
+				}
+			}
+		}
+	}
+}
+
+chrome.storage.sync.get(null, function(data) {
+	console.log(data);
+	for(elt of allElts){
+		if(elt.tagName !== "CODE" && elt.tagName !== "PRE" && elt.tagName !== "SPAN"){
+			if(data.darkmode && (elt.style.display === "block" || elt.style || elt.style.display ==="flex")){
+				elt.style["background-color"] = "#202020";
+				elt.style.backgroundColor = "#202020";
+			}
+			elt.style["color"] = data.txt;
+			elt.style.color = data.txt;
+			if(elt.style.border || elt.style["border-color"]){
+				elt.style["border-color"] = data.txt;
+			}
+		}
+		else if (elt.tagName === "CODE"){
+			elt.style["color"] = "black";
+			elt.style.color = "black";
+		}
+	}
+	//Applies selected color to body
+	body.style.backgroundColor = data.background;
+	body.style.color = data.text;
+	
+	//Applies selected color to head
+	document.getElementsByTagName('head')[0].style.backgroundColor = data.background;
+
+	if(data.layers){
+		divLayer(firstLayerDivs, 0, data.layers, data.background);
+	}
+	
+	//Applies selected color to text
+	body.style.color = data.txt;
+
+	//Applies selected color to header, moved to bottom because it sometimes fails due to CORS policy
+	document.getElementsByTagName('header')[0].style.backgroundColor = data.background;
+});
